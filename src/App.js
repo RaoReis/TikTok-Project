bb7d3fb18255481e8f61461ea56e683e8db86ec0
Raony Reis <raoreis20@gmail.com>
2023-04-01 16:05:47 -0300

    aula 4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name= "Paulo"
+          description= "Brecker o goleiro"
+          music= "musica épica"
+          url="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"
+        />
+        <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name= "Pedro"
+          description= "Bird olhando para a camera"
+          music= "Clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
